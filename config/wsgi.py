ff90d6e35576929296b6d5fac6ab6cdf9078042a
Nick Meyer <bierdybard@gmail.com>
2018-04-16 18:34:22 -0500

    initial project skeletons

---
+++ config/wsgi.py
@@ -0,0 +1,23 @@
+"""
+WSGI config for cuisinio project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+import sys
+
+from django.core.wsgi import get_wsgi_application
+
+# This allows easy placement of apps within the interior
+# backend directory.
+app_path = os.path.abspath(os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), os.pardir))
+sys.path.append(os.path.join(app_path, 'backend'))
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+application = get_wsgi_application()
